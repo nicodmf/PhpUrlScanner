df94cd5d63a8b3a1d96d2c87b1fcd1ae36147974
ob-ivan <ob-ivan@github.com>
2013-09-21 01:20:29 +0400

    Replace double quotes with single quotes for the great justice

--- scanner.php
+++ scanner.php
@@ -6,16 +6,16 @@ use Alinea\URLScanner\Scanner;
 if (isset($argc) && getcwd() == __DIR__) {
     if ($argc == 2) {
         Scanner::collect_and_return($argv[1]);
-    } elseif ($argc==3) {
+    } elseif ($argc == 3) {
         switch ($argv[1]) {
-            case "status":
-                echo Scanner::get_status($argv[2])."\n";
+            case 'status':
+                echo Scanner::get_status($argv[2]) . "\n";
                 break;
 
             default :
                 Scanner::collect_and_save($argv[1], $argv[2]);
         }
-    } elseif($argc > 3) {
+    } elseif ($argc > 3) {
         #Scanner::collect_many_and_save($argv[1]);
         $url = $argv[1];
         $file = $argv[2];
