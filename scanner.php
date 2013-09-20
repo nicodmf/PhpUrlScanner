e2778c25b4fb3a0023aac6784b0054c8b0790877
ob-ivan <ob-ivan@github.com>
2013-09-20 23:40:02 +0400

    Move LogDebug to separate namespaced file and fix codestyle to comply with PSR

--- scanner.php
+++ scanner.php
@@ -6,53 +6,8 @@ namespace Alinea\URLScanner;
 
 require_once __DIR__ . '/goutte.phar';
 
-
 use Goutte\Client;
 use ArrayObject;
-
-class LogDebug{
-	private $output = true;
-	private $log = false;
-	private $debug = false;
-	private $level = 1;
-	private $dir = "logs/";
-	private $domain_placement = 20;
-	static $instance = null;
-
-
-	private function __construct(){
-		//throw new Exception( "preg".preg_replace("#\\\#", "/", __NAMESPACE__ ));
-		$this->dir = __DIR__ ."/".$this->dir. preg_replace("#\\\#", "/", __NAMESPACE__ ) . "/";
-		if(!file_exists($this->dir))mkdir($this->dir, 0777, true);
-	}
-	static public function silent(){
-		self::get()->level = 0;
-		self::get()->output = false;
-		self::get()->log = false;
-		self::get()->debug = false;
-	}
-	static public function add($var, $domain="main", $level=1){
-		self::get()->make($var, $domain, $level);
-	}
-	static private function get(){
-		if(self::$instance==null)return self::$instance = new LogDebug();
-		return self::$instance;
-	}
-	private function make($var, $domain, $level){
-		if($this->level<$level) return;
-		if( !$this->debug && !$this->log && !$this->output)return;
-		//$head = sprint_f($this->model, $domain, 0, strlen($domain) );
-		if(!is_string($var))$var = print_r($var, true);
-		$output = $stdout = "";
-
-		if($this->debug){
-			foreach(preg_split("/\n/", $var) as $line){
-				printf("[ %-{$this->domain_placement}s%s ] * %s\n", $domain, date("c"), $line);
-			}
-		}elseif(!$this->debug && $this->output && $domain==="main") echo $var."\n";
-		if($this->log) file_put_contents($this->dir.$domain.".log", $output, \FILE_APPEND);
-	}
-}
 
 class URL{
 	public $scheme;
